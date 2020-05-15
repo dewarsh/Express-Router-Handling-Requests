0d9f917df54384ec2c8b0b771b91cdfb9088054a
dewarsh <dewarshjoshi505@gmail.com>
2020-05-15 21:47:50 +0530

    Handling POST request for creating a new member

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const members = require('./members')
 const app = express()
 
 // app.get('/', (req, res) => {
 //     console.log('Demo on handling http requests for URL /api/members')
 // })
+
+// Body parser because body of request Object was unable to read JSON
+app.use(express.json())
 
 // Members API routes
 app.use('/api/members', require('./routes/api/membersData'))
--- routes/api/membersData.js
+++ routes/api/membersData.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const members = require('../../members')
+const uuid = require('uuid')
+const Joi = require('joi')
 
 // Get All Members
 router.get('/', (req, res) => {
@@ -17,5 +19,32 @@ router.get('/:id', (req, res) => {
         res.status(400).json({ msg: `No member with the id of ${req.params.id}`})
     }
 })
+
+// Creating a new member
+router.post('/', (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: "active"
+    }
+
+    const { error } = validateMember(req.body)
+
+    if(!error) {
+        members.push(newMember)
+        res.json(members)
+    } else {
+        res.send(error.details[0].message)
+    }
+})
+
+function validateMember(member) {
+    const schema = {
+        name: Joi.string().min(3).required(),
+        email: Joi.string().email().required()
+    }
+    return Joi.validate(member, schema)
+}
 
 module.exports = router
