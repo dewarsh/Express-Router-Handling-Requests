089cbbcc2476b41c8a4a00c1a170f1ed2d84a0e3
dewarsh <dewarshjoshi505@gmail.com>
2020-05-15 17:58:47 +0530

    Created index.js and started the server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Demo on handling http requests for URL /api/members')
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Listening on port ${port}...`))
