c57c9a1de839311c4b655d63514ca8c5aa111db2
dewarsh <dewarshjoshi505@gmail.com>
2020-05-15 18:12:53 +0530

    Acquiring members and handling GET request for fetching all members

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const members = require('./members')
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log('Demo on handling http requests for URL /api/members')
+// app.get('/', (req, res) => {
+//     console.log('Demo on handling http requests for URL /api/members')
+// })
+
+// Get All Members
+app.get('/api/members', (req, res) => {
+    res.json(members)
 })
 
 const port = process.env.PORT || 5000
